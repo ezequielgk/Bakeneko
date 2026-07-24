cd78a59152b7bc9aca777ee3a1a7d3a09c405198
ezequielgk <rox737280@gmail.com>
2026-07-24 20:24:13 -0300

    chore: add libglvnd and libepoxy to shell environment dependencies and LD_LIBRARY_PATH

--- shell.nix
+++ shell.nix
@@ -29,6 +29,7 @@ pkgs.mkShell {
     harfbuzz
     freetype
     libGL
+    libglvnd
 
     # --- Flutter & Dart ---
     flutter
@@ -55,7 +56,7 @@ pkgs.mkShell {
     # En build empaquetado lo provee sqlite3_flutter_libs junto al binario.
     # stdenv.cc.cc.lib provee libstdc++.so.6 que el binario Flutter precompilado
     # carga en runtime pero nix-shell no expone por defecto.
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.sqlite.out}/lib:${pkgs.libGL}/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.sqlite.out}/lib:${pkgs.libGL}/lib:${pkgs.libglvnd}/lib:${pkgs.libepoxy}/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
     echo ""
     echo "  bakeneko dev shell"
     echo "  flutter:  $(flutter --version 2>/dev/null | head -1 | sed 's/^[«"]//; s/[»"].*//')"
